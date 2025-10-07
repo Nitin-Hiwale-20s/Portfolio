a7c7ff0ce75ea0f5107c90ca2ac259f24023f001
Nitn Rajesh Hiwale <nitinhiwale67@gmail.com>
2025-10-07 14:15:21 +0530

    Hero.jsx

--- Server/src/components/Hero.jsx
+++ Server/src/components/Hero.jsx
@@ -1,5 +1,3 @@
-
-
 import React from "react";
 import "./Hero.css";
 import PortfolioBack from "../assets/Black1.jpg";
@@ -7,23 +5,21 @@ import PortfolioBack from "../assets/Black1.jpg";
 const Hero = () => {
   return (
     <div
-      className="relative h-[700px] bg-cover bg-center flex items-center justify-center text-center"
+      className="relative h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center text-center"
       style={{ backgroundImage: `url(${PortfolioBack})` }}
     >
-      <div className="container">
+      <div className="container text-white">
         <h1 className="welcome">WELCOME</h1>
         <p className="iam">I AM</p>
         <h2 className="name">Nitin</h2>
         <h2 className="surname">Hiwale</h2>
 
-        <div className="underline"></div>
+        <div className="underline mx-auto my-4 w-24 h-1 bg-white"></div>
 
         <p className="role">
           Full-Stack Developer | MERN Stack | Java & DSA Enthusiast <br />
           JavaScript & React.js | 3rd Year Computer Science Student!
         </p>
-
-      
       </div>
     </div>
   );
